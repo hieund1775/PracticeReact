6c692f8c32b8376c098ba732552cf378e86c9593
Nguyen Duong Hieu <hieund1775@gmail.com>
2026-07-04 22:22:19 +0700

    Learn Props React, Relationshop Father and son

--- LearnReact/src/App.jsx
+++ LearnReact/src/App.jsx
@@ -2,12 +2,19 @@ import "./components/todo/todo.css";
 import TodoData from "./components/todo/TodoData";
 import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/hero.png";
+
 const App = () => {
+  const hieune = "Eric Arsenal";
+  const age = 25;
+  const data = {
+    address: "hanoi",
+    country: "vietnam",
+  };
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData name={hieune} age={age} data={data} />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>
--- LearnReact/src/components/todo/TodoData.jsx
+++ LearnReact/src/components/todo/TodoData.jsx
@@ -1,6 +1,24 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  // props là một object {}
+  // {
+  //   name: "Eric",
+  //   age: 25,
+  //   data:{}
+  // }
+  // Object destructuring": học cú pháp này
+  // khai báo như sau:
+  const { name, age, data } = props;
+  // ==========
+  // cách code của beginer
+  // const name = props.name;
+  // const age = props.age;
+  // const data = props.data;
+
+  console.log(">>>> check props: ", props);
   return (
     <div className="todo-data">
+      <div>My name is {props.name}</div>
+      {/* lấy data từ component cha để sử dụng */}
       <div>Learning React</div>
       <div>Watching Youtube</div>
     </div>

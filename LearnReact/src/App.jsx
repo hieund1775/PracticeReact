7d171c063ab97fd1fbb30afb55086ae6dd5ff195
Nguyen Duong Hieu <hieund1775@gmail.com>
2026-07-08 18:06:10 +0700

    Learn Give Data Father to Son

--- LearnReact/src/App.jsx
+++ LearnReact/src/App.jsx
@@ -10,10 +10,16 @@ const App = () => {
     address: "hanoi",
     country: "vietnam",
   };
+
+  const addNewTodo = (name) => {
+    alert(`call me ${name}`); //dấu backtips
+    // truyền function từ cha sang con
+  };
+  addNewTodo();
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
-      <TodoNew />
+      <TodoNew addNewTodo={addNewTodo} />
       <TodoData name={hieune} age={age} data={data} />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
